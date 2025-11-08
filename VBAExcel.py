280c18ef1c9ef71727e7c7a964ce1f93d2da360c
martin123-oke <65245583+martin123-oke@users.noreply.github.com>
2025-11-08 12:18:04 +0700

    Update VBAExcel.py

--- VBAExcel.py
+++ VBAExcel.py
@@ -153,11 +153,82 @@ Range("j2").Interior.Color = vbWhite
 Range("j2").Value = ""
 End Sub
     ''')
-    st.subheader("Ruang Diskusi")
-    st.markdown('''
-                    <iframe src="https://martin123-oke.github.io/media/diskusi1.html" style="width:100%; height:1000px"></iframe>
-                    ''',unsafe_allow_html=True)
+     
+def materi3():
+    st.header("Hasil Diskusi")
+    st.markdown("<iframe src='https://martin123-oke.github.io/PengVBAExcel/hasilDiskusi.html' style='width:100%;height:800px'></iframe>",unsafe_allow_html=True)
     
+
+def materi4():
+    st.header("Pecahan Sederhana")
+    st.markdown("<iframe src='https://res.cloudinary.com/dfkw4ux0e/image/upload/v1762577355/Pecahan_p1zm2o.png' style='width:100%;height:300px'></iframe>",unsafe_allow_html=True)
+    with st.expander("Konsep Dasar For"):
+        st.write("Konsep Dasar")
+        st.markdown('''
+            For digunakan untuk melakukan perulangan (loop) dengan jumlah iterasi yang sudah ditentukan.
+👉 Struktur dasarnya di VBA:
+        ''')
+        st.code('''
+            For i = nilai_awal To nilai_akhir [Step nilai_langkah]
+    'blok perintah yang diulang
+Next i
+        ''')
+        st.markdown('''
+- i → variabel penghitung (counter)
+- To → batas akhir perulangan
+- Step → besar kenaikan/penurunan tiap iterasi (default = 1)
+''')
+    with st.expander("Contoh Dasar"):
+        st.write("Contoh Dasar")
+        st.code('''
+            Sub LoopAngka()
+    Dim i As Integer
+    For i = 1 To 5
+        MsgBox "Angka ke-" & i
+    Next i
+End Sub
+        ''')
+        st.markdown('''
+📘 Penjelasan:
+- Program menampilkan pesan 5 kali: "Angka ke-1", "Angka ke-2", dst.
+- For i = 1 To 5 → dimulai dari 1 hingga 5.
+''')
+    st.code('''
+    On Error Resume Next
+Dim lembar As Worksheet
+Dim gambar As Shape
+
+'mengaktifkan lembar
+Set lembar = Worksheets("pecahan")
+
+'memastikan posisi dan ukuran
+posisix = lembar.Shapes("kotak").Left
+posisiy = lembar.Shapes("kotak").Top
+panjang = lembar.Shapes("kotak").Width
+lebar = lembar.Shapes("kotak").Height
+
+'hapus objek yang masih ada
+For i = 1 To 200
+    lembar.Shapes("kotak" & i).Delete
+Next i
+
+'menggambakan penyebut
+For i = 1 To Range("B3").Value
+    Set gambar = lembar.Shapes.AddShape(msoShapeRectangle, posisix, posisiy + (i - 1) * lebar / Range("B3").Value, panjang, lebar / Range("B3").Value)
+    gambar.Name = "kotak" & i
+    lembar.Shapes("kotak" & i).Fill.ForeColor.RGB = vbWhite
+    lembar.Shapes("kotak" & i).Line.ForeColor.RGB = vbBlack
+    lembar.Shapes("kotah" & i).Line.Weight = 2
+Next i
+
+'Asiran untuk pembilang
+For i = 1 To Range("B2")
+    lembar.Shapes("kotak" & Range("B3") - (i - 1)).Fill.Patterned msoPatternDarkUpwardDiagonal
+    lembar.Shapes("kotak" & Range("B3") - (i - 1)).Fill.ForeColor.RGB = vbBlack
+Next i
+End Sub
+    ''')
+
 
 if st.session_state.kondisi['kondisi1']:
     kover()
@@ -165,6 +236,10 @@ if st.session_state.kondisi['kondisi2']:
     materi1()
 if st.session_state.kondisi['kondisi3']:
     materi2()
+if st.session_state.kondisi['kondisi4']:
+    materi3()
+if st.session_state.kondisi['kondisi5']:
+    materi4()
 
 if st.sidebar.button('Beranda'):
     st.session_state['kondisi']={'kondisi1':True,'kondisi2':False,
@@ -182,3 +257,18 @@ if st.sidebar.button("Kalkulator Sederhana"):
                                  'kondisi3':True,'kondisi4':False,
                                  'kondisi5':False}
     st.rerun()
+if st.sidebar.button("Pecahan Sederhana"):
+    st.session_state['kondisi']={'kondisi1':False,'kondisi2':False,
+                                 'kondisi3':False,'kondisi4':False,
+                                 'kondisi5':True}
+    st.rerun()
+if st.sidebar.button("Lihat Media Hasil Diskusi"):
+    st.session_state['kondisi']={'kondisi1':False,'kondisi2':False,
+                                 'kondisi3':False,'kondisi4':True,
+                                 'kondisi5':False}
+    st.rerun()
+
+st.subheader("Ruang Diskusi")
+st.markdown('''
+                <iframe src="https://martin123-oke.github.io/media/diskusi1.html" style="width:100%; height:1000px"></iframe>
+            ''',unsafe_allow_html=True)
